6bf1da23c99024938dcbd061742e1916fa543459
Jswears <ji.swearssalinas@gmail.com>
2025-01-27 11:37:20 +0100

    fix: skeleton, and remove unecessary imports

--- app/wines/components/ProductCard.tsx
+++ app/wines/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { WineCardProps } from '@/app/types/components';
 import { priceConversor } from '@/app/utils/priceConversor';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
 
