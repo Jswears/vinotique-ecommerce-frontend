f39c06f095a521a4bb724d73684aa36b61021ee8
Jswears <ji.swearssalinas@gmail.com>
2025-02-22 12:03:10 +0100

    fix: File reference error

--- app/admin/add-wine/components/ImagePreview.tsx
+++ app/admin/add-wine/components/ImagePreview.tsx
@@ -16,15 +16,14 @@ export default function ImagePreview({ previewUrl }: ImagePreviewProps) {
         )
     }
 
-    <div className="mt-2">
-        <Image src={previewUrl} alt="Preview" width={100} height={100} className="rounded-md h-52 w-fit" />
-    </div>
-
     return (
         <Card>
             <CardContent className="p-0 overflow-hidden">
                 <div className="flex items-center justify-center h-48 bg-gray-100 dark:bg-gray-800">
                     <Image src={previewUrl || "/placeholder.svg"} alt="Wine preview" width={100} height={100} className="rounded-md h-52 w-fit" />
+                </div>
+                <div className="mt-2">
+                    <Image src={previewUrl} alt="Preview" width={100} height={100} className="rounded-md h-52 w-fit" />
                 </div>
             </CardContent>
         </Card>
