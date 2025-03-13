f3f024f01db30259a8a53e0d68a7043d108e421a
Jswears <ji.swearssalinas@gmail.com>
2025-03-13 09:25:51 +0100

    fix: Link

--- components/home/CallToAction.tsx
+++ components/home/CallToAction.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const CallToAction = () => {
     return (
         <div className="w-full flex flex-col items-center justify-center bg-orange-600 p-8 rounded-lg mb-16">
@@ -7,12 +9,13 @@ const CallToAction = () => {
             <p className="text-white text-center mt-2">
                 Visit our full collection to find your perfect wine.
             </p>
-            <a
+            {/* Fix */}
+            <Link
                 href="/wines"
                 className="mt-6 px-6 py-3 bg-black text-white rounded-full hover:bg-gray-800 transition"
             >
                 View All Wines →
-            </a>
+            </Link>
         </div>
     );
 };
